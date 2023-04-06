23d95ed04cf10d0f754868d3457804607ea34a21
Haseeb Iqbal <uusamaimran@gmail.com>
2023-04-06 14:18:27 +0500

    Api strucure using react query

---
+++ src/api/usePostsApi.ts
@@ -0,0 +1,32 @@
+import { useMutation, useQuery } from 'react-query'
+import { useApi } from './Api'
+
+const POSTS_BASE_API_ENDPOINT = 'https://jsonplaceholder.typicode.com/posts'
+
+export const usePosts = () => {
+    const Api = useApi()
+
+    return useQuery({
+        queryKey: 'posts',
+        queryFn: () => Api.get<unknown[]>(POSTS_BASE_API_ENDPOINT),
+        enabled: true,
+    })
+}
+
+export const useGetSinglePost = (id: string) => {
+    const Api = useApi()
+    return useQuery({
+        queryKey: ['posts', 'singlePost', id],
+        queryFn: () => Api.get(`${POSTS_BASE_API_ENDPOINT}/${id}`),
+        enabled: !!id,
+    })
+}
+
+export const useCreatePost = () => {
+    const Api = useApi()
+    return useMutation({
+        mutationKey: 'create Post',
+        mutationFn: (body: Record<string, unknown>) =>
+            Api.post(POSTS_BASE_API_ENDPOINT, body),
+    })
+}
