fdf1a413016c6b044c39b53fc316d43d945e9c7b
Usama Imran <uusamaimran@gmail.com>
2024-06-25 21:48:08 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { usePosts } from './api/usePostsApi'
+import { usePosts } from "./api/usePostsApi";
 
 function App() {
-    const { data } = usePosts()
+  const { data } = usePosts();
 
-    return (
-        <div className="App">
-            <h1>API Structure Testing</h1>
-            {JSON.stringify(data?.length)}
-            <hr />
-            {JSON.stringify(data?.map((item: any) => item.title))}
-        </div>
-    )
+  return (
+    <div className="App">
+      <h1>API Structure Testing</h1>
+      {JSON.stringify(data?.length)}
+      <hr />
+      {JSON.stringify(data?.map((item: any) => item.title))}
+    </div>
+  );
 }
 
-export default App
+export default App;
